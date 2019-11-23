6e7d2a16d780abdab2413bca522f8478a168b9db
Tamalera <florence.meier@alumni.ethz.ch>
2019-11-23 14:36:12 +0100

    Add radar chart to detail page

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,17 +4,31 @@
     <div class="columns">
       <div class="column">
         <img src="../assets/placeholder.png" />
-        <div>HERE FACTS</div>
-      </div>
-      <div class="column">
-        CHART
-      </div>
-      <div class="column">
         <div>
-          NEWS
+          <b-table :data="data" :columns="columns"></b-table>
         </div>
+      </div>
+      <div class="column">
+        <radar :chart-data="datacollection"></radar>
+      </div>
+      <div class="column">
         <div>
-          LEAKS
+          <b-menu>
+            <b-menu-list label="News">
+              <b-menu-item label="News Item 1"></b-menu-item>
+              <b-menu-item label="News Item 1"></b-menu-item>
+              <b-menu-item label="News Item 1"></b-menu-item>
+            </b-menu-list>
+          </b-menu>
+        </div>
+        <div class="shift-down">
+          <b-menu>
+            <b-menu-list label="Leaks">
+              <b-menu-item label="Leak Item 1"></b-menu-item>
+              <b-menu-item label="Leak Item 1"></b-menu-item>
+              <b-menu-item label="Leak Item 1"></b-menu-item>
+            </b-menu-list>
+          </b-menu>
         </div>
       </div>
     </div>
@@ -22,7 +36,81 @@
 </template>
 
 <script>
+import Radar from "../components/Radar.vue";
+
 export default {
-  name: "about"
+  name: "about",
+  components: {
+    Radar
+  },
+  data() {
+    return {
+      datacollection: null,
+      data: [
+        {
+          id: "Type",
+          facts: "Pfarmaceutical"
+        },
+        {
+          id: "Asset",
+          facts: "200M CHF"
+        },
+        {
+          id: "Revenue",
+          facts: "500M CHF"
+        },
+        {
+          id: "#Employees",
+          facts: "150'000"
+        },
+        {
+          id: "CEO",
+          facts: "Pikachu"
+        }
+      ],
+      columns: [
+        {
+          field: "id",
+          label: "Id",
+          width: "40"
+        },
+        {
+          field: "facts",
+          label: "Fact"
+        }
+      ]
+    };
+  },
+  mounted() {
+    this.fillData();
+  },
+  methods: {
+    fillData() {
+      this.datacollection = {
+        labels: [this.getRandomInt(), this.getRandomInt()],
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: "#f87979",
+            data: [this.getRandomInt(), this.getRandomInt()]
+          },
+          {
+            label: "Data One",
+            backgroundColor: "#f87979",
+            data: [this.getRandomInt(), this.getRandomInt()]
+          }
+        ]
+      };
+    },
+    getRandomInt() {
+      return Math.floor(Math.random() * (50 - 5 + 1)) + 5;
+    }
+  }
 };
 </script>
+
+<style scoped>
+.shift-down {
+  margin-top: 16px;
+}
+</style>
